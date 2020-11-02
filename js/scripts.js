3d43cb5bf113d0688d93e111783f88a0d5b5d803
Kaila Sprague McRae <kaila.sprague@icloud.com>
2020-11-02 11:51:10 -0800

    pass Places test 1, function successfully created
    Co-authored-by: Brian Sturgis <sturujisu@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,12 @@
 //bussiness logic
+// Describe: Place();
 // Test: It will create a Constructor for Place containing different properties
 // Expect: Place().toEqual(location, landmarks, time of year, notes);
 
-
-
+function Places(location, landmark, timeOfYear, notes){
+  this.location = location;
+  this.landmark = landmark;
+  this.timeOfYear = timeOfYear;
+  this.notes = notes;
+}
 //user interface logic
